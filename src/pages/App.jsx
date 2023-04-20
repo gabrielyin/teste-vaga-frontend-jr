d8cda55b37e9d7f3f8d3db732842963b9265aad5
gabrielyin <2021gabrielyin@gmail.com>
2023-04-19 22:22:46 -0300

    card slider container

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -96,47 +96,104 @@ function App() {
         </div>
       </section>
       <section className={style.categoryContainer}>
-        <div className={style.categoryContainer__card}>
+        <div className={style.categoryContainer__card__active}>
           <div>
-            <img src="" alt="" />
+            <img src="assets/icons/MonitorSmartphoneTablet.svg" alt="" />
           </div>
           <h5>Tecnologia</h5>
         </div>
         <div className={style.categoryContainer__card}>
           <div>
-            <img src="" alt="" />
+            <img src="assets/icons/Supermercado.svg" alt="" />
           </div>
           <h5>Supermercado</h5>
         </div>
         <div className={style.categoryContainer__card}>
           <div>
-            <img src="" alt="" />
+            <img src="assets/icons/Whiskey.svg" alt="" />
           </div>
           <h5>Bebidas</h5>
         </div>
         <div className={style.categoryContainer__card}>
           <div>
-            <img src="" alt="" />
+            <img src="assets/icons/Ferramenta.svg" alt="" />
           </div>
           <h5>Ferramentas</h5>
         </div>
         <div className={style.categoryContainer__card}>
           <div>
-            <img src="" alt="" />
+            <img src="assets/icons/Saude.svg" alt="" />
           </div>
           <h5>Saúde</h5>
         </div>
         <div className={style.categoryContainer__card}>
           <div>
-            <img src="" alt="" />
+            <img src="assets/icons/Corrida.svg" alt="" />
           </div>
           <h5>Esportes e Fitness</h5>
         </div>
         <div className={style.categoryContainer__card}>
           <div>
-            <img src="" alt="" />
+            <img src="assets/icons/Moda.svg" alt="" />
           </div>
           <h5>Moda</h5>
+        </div>
+      </section>
+      <section className={style.relacionadosContainer}>
+        <div>
+          <div className={style.relacionadosContainer__title}>
+            <hr />
+            <h2>Produtos relacionados</h2>
+            <hr />
+          </div>
+          <nav className={style.relacionadosContainer__nav}>
+            <div>
+              <a>celular</a>
+            </div>
+            <div>
+              <a>acessórios</a>
+            </div>
+            <div>
+              <a>tablets</a>
+            </div>
+            <div>
+              <a>notebooks</a>
+            </div>
+            <div>
+              <a>tvs</a>
+            </div>
+            <div>
+              <a>ver todos</a>
+            </div>
+          </nav>
+          <div className={style.cardContainer}>
+            <div className={style.cardContainer__main}>
+              <div className={style.cardContainer__main__card}>
+                <div className={style.cardContainer__main__card__content}>
+                  <img src="https://app.econverse.com.br/teste-front-end/junior/tecnologia/fotos-produtos/foto-iphone.png" alt="" />
+                  <div>
+                    <p>Iphone 11 PRO MAX BRANCO 1</p>
+                    <h6>R$ 30,00</h6>
+                    <h5>R$ 15000</h5>
+                    <p>ou 2x de R4 49,95 sem juros</p>
+                    <p>Frete grátis</p>
+                  </div>
+                  <button>
+                    comprar
+                  </button>
+                </div>
+              </div>
+              <div className={style.cardContainer__main__card}>
+
+              </div>
+              <div className={style.cardContainer__main__card}>
+
+              </div>
+              <div className={style.cardContainer__main__card}>
+
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
